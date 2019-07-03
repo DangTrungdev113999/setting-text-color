e57d941614ecea91a4275c07d578aaee4e9803fa
dangtrungdev113999 <dangtrungdev113999@gmail.com>
2019-07-03 19:21:57 +0700

    change color

--- src/App.js
+++ src/App.js
@@ -6,16 +6,34 @@ import Result from './components/Result';
 import Reset from './components/Reset';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: 'red',
+            fontSize: 15
+        }
+    }
+
+    onSetColor = (param) => {
+        this.setState({
+            color: param
+        })
+    }
+
     render() {
         return(
             <div className="container mt-50">
                 <div className="row">
-                    <ColorPicker/>
+                    <ColorPicker 
+                        color={ this.state.color } 
+                        onReceiveColor={ this.onSetColor } 
+                    />
+
                     <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                        <SizeSetting/>
+                        <SizeSetting />
                         <Reset/>
                     </div>
-                    <Result/>
+                    <Result color= {this.state.color} />
                 </div>
             </div>
         );
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
 class Result extends Component {
+
+
+    setStyle() {
+        return {
+            color: this.props.color,
+            borderColor: this.props.color
+        }
+    }
+
     render() {
         return(
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <p> color: red - fontzie: 15 </p>
-                <div id='content'>
+                <div id='content' style={ this.setStyle() }>
                     nội dung setting
                 </div>
             </div>
